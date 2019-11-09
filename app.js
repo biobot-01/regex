721dea5c0c01124e521165b23163f9171b9c7796
Kevin Michael <biobot.mike@gmail.com>
2019-11-09 19:27:31 +0200

    Examples of metacharacter symbols in regex

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 let re;
+// Literal Characters
 re = /hello/;
-re = /hello/i; // i = case insensitive
-// re = /hello/g; // Global search
-// console.log(re);
-// console.log(re.source);
+re = /hello/i;
 
-// exec() - Return result in an array or null
-// const result = re.exec('hello world');
-// console.log(result);
-// console.log(result[0]);
-// console.log(result.index);
-// console.log(result.input);
+// Metacharacter Symbols
+re = /^h/i; // Must start with
+re = /world$/i; // Must ends with
+re = /^hello$/i; // Must begin & end with
+re = /h.llo/i; // Matches any ONE character
+re = /h*llo/i; // Matches any character 0 or more times
+re = /gre?a?y/i; // Optional character
+re = /gre?a?y\?/i; // Escape character
 
-// test() - Returns true or false
-// const result = re.test('Hello'); 
-// console.log(result);
+// String to match
+const str = 'Gray?';
 
-// match() - Return result array or null
-// const str = 'Hello There';
-// const result = str.match(re);
-// console.log(result);
+// Log results
+const result = re.exec(str);
+console.log(result);
 
-// search() - Returns index of the first match if not found returns -1
-// const str = 'Hello There';
-// const result = str.search(re);
-// console.log(result);
+function reTest(re, str) {
+  if (re.test(str)) {
+    console.log(`${str} matches ${re.source}`);
+  } else {
+    console.log(`${str} does NOT match ${re.source}`);
+  }
+}
 
-// replace() - Return new string with some or all matches of a pattern
-// const str = 'Hello There';
-// const newStr = str.replace(re, 'Hi');
-// console.log(newStr);
+reTest(re, str);
